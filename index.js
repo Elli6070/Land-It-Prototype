91268a53a71628e06b8bf240d605bc8a69e3fe26
Drew Elliott <elli6070@stthomas.edu>
2019-11-20 10:14:11 -0600

    Added socket functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var express = require("express");
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -12,20 +14,24 @@ if (port == null || port == "") {
 var index_file = path.join(__dirname, "Index.html");
 
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     console.log("Getting Index.html at " + index_file);
 
     fs.readFile(index_file, (err, index) => {
         if (err) { console.log("Error getting Index.html"); }
 
         else {
-			res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(index);
-            res.end();
+			res.sendFile(index_file);
         }
     });
-    
 
 });
 
-app.listen(port, () => console.log("App now listening on port " + port));
+io.on('connection', function(socket){
+  socket.on('print message', function(msg){
+    console.log('sending message: ' + msg + 'to printer');
+	io.emit('print message', msg);
+  });
+});
+
+http.listen(port, () => console.log("App now listening on port " + port));
